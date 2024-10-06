6fc462cd22e87ddc7aac827531be911b18933938
Ricardo Ramirez <6ricardo6ramirez6@gmail.com>
2024-10-06 01:18:20 -0300

    v 0.0.22 general styles added

--- src/app/common-view/common-v-routing.module.ts
+++ src/app/common-view/common-v-routing.module.ts
@@ -6,7 +6,7 @@ import { CommonViewComponent } from './common-view.component';
 
 
 const routes: Routes = [
-  {path:'principal', component:PrincipalPageComponent},
+  // {path:'principal', component:PrincipalPageComponent},
   {path:'', component:CommonViewComponent, children:[
     { path: 'base',  component: PrincipalPageComponent }
   ]},
--- src/app/principal-page/principal-page.component.ts
+++ src/app/principal-page/principal-page.component.ts
@@ -5,89 +5,81 @@ import { LocalStorageService } from '../core/services/local-storage.service';
 @Component({
   selector: 'app-principal-page',
   templateUrl: './principal-page.component.html',
-  styleUrls: ['./principal-page.component.css']
+  styleUrls: ['./principal-page.component.css'],
 })
 export class PrincipalPageComponent implements OnInit {
-
-// Para la versión 0.0.17 se deja de utilizar observables luego del local storage
+  // Para la versión 0.0.17 se deja de utilizar observables luego del local storage
 
   constructor(
     // private loginSv:LoginService,
-    private router:Router,
-    private localStorageSv:LocalStorageService
+    private router: Router,
+    private localStorageSv: LocalStorageService
   ) {
     // this.user$ = loginSv.observableUser;
     // this.user$.subscribe(data =>{
-    
-      // para definir las headlines
-      // this.headlines = data.headlines;
-      // para definir las tasks (REVISAR) (queda feo)
-      // this.tasks = this.headlines[0].tasks as unknown as Task[];
-      // this.taskTittles = this.tasks.forEach((tittle) => tittle.content) as unknown as string[]
+
+    // para definir las headlines
+    // this.headlines = data.headlines;
+    // para definir las tasks (REVISAR) (queda feo)
+    // this.tasks = this.headlines[0].tasks as unknown as Task[];
+    // this.taskTittles = this.tasks.forEach((tittle) => tittle.content) as unknown as string[]
     // } );
 
     // this.user=localStorageSv.getUser
     // this.headlines=this.user.headlines
-    
-    if (!this.localStorageSv.getLogin){
-      this.redirect(this.urlLogin)
+
+    if (!this.localStorageSv.getLogin) {
+      this.redirect(this.urlLogin);
     }
-
-    
-
   }
-  
-  ngOnInit(): void {
-  }
-  
+
+  ngOnInit(): void { }
+
   // user$:Observable<User>
 
   // --------------------------------- Url -------------------------------------
   private urlLogin = '/login';
 
   // ------------------------------ Variables ----------------------------------
-  displayN:string="display1"
-  expand:boolean=false;
-  display:string[]=["","","",""];
-  displayM:string[]=["","displayNoneM","displayNoneM","displayNoneM"];
-  
-  
+  displayN: string = 'display1';
+  expand: boolean = false;
+  display: string[] = ['', '', '', ''];
+  displayM: string[] = ['', 'displayNoneM', 'displayNoneM', 'displayNoneM'];
+
   // user!:User;
   // headlines!:Headline[];
   // tasks!:Task[];
   // taskTittles!:string[];
   // ---------------------------------------------------------------------------
-  
-  
 
+  changeExpand() {
+    this.expand = !this.expand;
+  }
 
-
-  changeExpand(){
-    this.expand = !this.expand
-  };
-  
-  changeDisplay(n:number){
-    if(n == 1){
-      this.display = ["","","",""];
-      this.displayM = ["","displayNoneM","displayNoneM","displayNoneM"];
-
+  changeDisplay(n: number) {
+    if (n == 1) {
+      this.display = ['', '', '', ''];
+      this.displayM = ['', 'displayNoneM', 'displayNoneM', 'displayNoneM'];
     }
-    this.displayN = "display"+n;
-    console.log(this.displayN)
+    this.displayN = 'display' + n;
+    console.log(this.displayN);
   }
 
-  changeCard(n:number){
-    this.display = ["displayNone","displayNone","displayNone","displayNone"]
-    this.display[n] = "";
-  };
-  changeCardM(n:number){
-    this.display = ["displayNoneM","displayNoneM","displayNoneM","displayNoneM"]
-    this.displayM[n] = ""
-  };
-  
-  redirect(path:string){
-    this.router.navigate([path])
+  changeCard(n: number) {
+    this.display = ['displayNone', 'displayNone', 'displayNone', 'displayNone'];
+    this.display[n] = '';
+  }
+  changeCardM(n: number) {
+    this.display = [
+      'displayNoneM',
+      'displayNoneM',
+      'displayNoneM',
+      'displayNoneM',
+    ];
+    this.displayM[n] = '';
   }
 
-
+  redirect(path: string) {
+    this.router.navigate([path]);
+  }
 }
